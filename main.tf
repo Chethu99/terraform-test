73a78406d634f2d3d05c4eb5822a50ca91a3fb19
Chethan Puthran <chethan.puthran@coupa.com>
2026-01-03 12:44:48 +0530

    add github actions terraform pipeline

--- main.tf
+++ main.tf
@@ -1,8 +1,21 @@
 terraform {
   required_version = ">= 1.0"
+
+backend "s3" {
+  bucket         = "chethan-tf-state-12345"
+  key            = "terraform-test/terraform.tfstate"
+  region         = "ap-south-1"
+  dynamodb_table = "terraform-locks-test"
+}
+}
+
+
+terraform {
+  required_version = ">= 1.0"
 }
 
 resource "local_file" "hello" {
   filename = "hello.txt"
-  content  = "Hello Terraform 👶"
+  content  = "THIS IS VERSION 3 🚀"
 }
+
